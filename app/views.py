f2fab1e3b951c4c21b5b3d065e9584c831a902ee
Sam Kreter <samkreter@gmail.com>
2016-03-10 14:33:48 -0600

    added homepage to the mix

--- app/views.py
+++ app/views.py
@@ -7,13 +7,17 @@ from .forms import LoginForm
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    return render_template('index.html')
 
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for OpenID="%s", remember_me=%s' %
+              (form.openid.data, str(form.remember_me.data)))
+        return redirect('/index')
     return render_template('login.html',
                            title='Sign In',
                            form=form)
