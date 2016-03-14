ed41e18c8758bb25331d6883f1440b8824c50581
Sam Kreter <samkreter@gmail.com>
2016-03-14 11:18:23 -0500

    fixed form posting

--- app/views.py
+++ app/views.py
@@ -1,6 +1,8 @@
 from flask import render_template, flash, redirect
 from app import app
+from app.models import User
 from .forms import LoginForm
+from app import db
 
 
 
@@ -22,8 +24,9 @@ def index():
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for OpenID="%s", remember_me=%s' %
-              (form.openid.data, str(form.remember_me.data)))
+        user = User(first_name=form.first_name.data,last_name=form.last_name.data,email=form.email.data
+        db.sessoin.add(user)
+        db.session.commit()
         return redirect('/index')
     return render_template('login.html',
                            title='Sign In',
