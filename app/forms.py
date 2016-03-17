ebe9d6ac6a30a73ff3d999def0945258e1a2f133
Sam Kreter <samkreter@gmail.com>
2016-03-17 16:38:35 -0500

    added formupload and error messages

--- app/forms.py
+++ app/forms.py
@@ -1,9 +1,11 @@
 from flask.ext.wtf import Form
 from wtforms import BooleanField, TextField, PasswordField, validators
 from wtforms.validators import DataRequired
+from flask_wtf.file import FileField
 
 class LoginForm(Form):
     first_name = TextField('first_name', validators=[DataRequired()])
     last_name = TextField('first_name', validators=[DataRequired()])
     email = TextField('Email Address', [validators.Length(min=6, max=35)])
+    resume = FileField('resume')
     remember_me = BooleanField('remember_me', default=False)
--- app/views.py
+++ app/views.py
@@ -1,33 +1,47 @@
-from flask import render_template, flash, redirect
+from flask import render_template, flash, redirect,request
 from app import app
 from app.models import User
 from .forms import LoginForm
 from app import db
+import os
 
 
 
 @app.route('/')
-@app.route('/index')
+@app.route('/index',methods=['GET', 'POST'])
 def index():
     form = LoginForm()
-    if form.validate_on_submit():
-        flash('Login requested for OpenID="%s", remember_me=%s' %
-              (form.openid.data, str(form.remember_me.data)))
-        return redirect('/index')
+    error = None
     return render_template('index.html',
-                           title='Sign In',
-                           form=form)
+                           title='Index',
+                           form=form,
+                           error=error)
 
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    form = LoginForm()
+    form = LoginForm(request.form)
+    error = None
+    success = None
     if form.validate_on_submit():
-        user = User(first_name=form.first_name.data,last_name=form.last_name.data,email=form.email.data
-        db.sessoin.add(user)
-        db.session.commit()
-        return redirect('/index')
-    return render_template('login.html',
-                           title='Sign In',
-                           form=form)
+        user = User(first_name=form.first_name.data,last_name=form.last_name.data,email=form.email.data)
+        filePath = os.path.join(app.config['UPLOAD_FOLDER'],form.email.data)
+        f = request.files['resume']
+        if f:
+            f.save(filePath)
+            db.sessoin.add(user)
+            db.session.commit()
+            success = "Application Complete, Thank You"
+            return render_template('index.html',
+                           title='Index',
+                           form=form,
+                           error=error,
+                           success=success)
+        error = "Failed to save your file"
+    else:
+        error = "Failed to Validate. Check your information"
+    return render_template('index.html',
+                           title='Index',
+                           form=form,
+                           error=error)
