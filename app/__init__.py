53eee05247f3c6b561333d697d3882c3c3f8423e
Sam Kreter <samkreter@gmail.com>
2016-03-18 10:45:30 -0500

    adding logging

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
-
+import logging
 
 app = Flask(__name__)
 app.config.from_object('config')
+app.logger.addHandler(logging.StreamHandler(sys.stdout))
+app.logger.setLevel(logging.ERROR)
 
 db = SQLAlchemy(app)
 
