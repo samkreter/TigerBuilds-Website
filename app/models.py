121b54cf1eb186540800572dfb18222cff66294a
Sam Kreter <samkreter@gmail.com>
2016-03-19 14:20:23 -0500

    added upgrade to the database

--- app/models.py
+++ app/models.py
@@ -5,7 +5,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(64), index=True)
     last_name = db.Column(db.String(64), index=True)
-    email = db.Column(db.String(120), index=True, unique=True)
+    email = db.Column(db.String(120), unique=True, index=True, nullable=False)
     posts = db.relationship('Post', backref='author', lazy='dynamic')
 
     def __repr__(self):
