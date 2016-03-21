2f065551bde8aba3fbab1df0e80d07041e2c5a4a
Sam Kreter <samkreter@gmail.com>
2016-03-20 22:29:11 -0500

    added new database config

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ SECRET_KEY = 'ou-will-never-guess'
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 #SQLALCHEMY_DATABASE_URI = "postgres://samkreter@localhost/tigerbuilds"
-SQLALCHEMY_DATABASE_URI = "postgres://sxpkcgcuxxjjxg:EDESil4B-dUZHxo-1Fif8pwkU9@ec2-54-83-56-177.compute-1.amazonaws.com:5432/da0if8ihlmlroa"
+SQLALCHEMY_DATABASE_URI = "postgres://kfavinwqcaxhtw:fbVKJHh7sI3qUcV51vfO6oE5wW@ec2-107-22-246-250.compute-1.amazonaws.com:5432/d7g39qrh0pv1l2"
 FILESTOREK = "AKIAJSFCV66627WSDHBQ"
 FILESTOREKS = "xf2lCBd4jUa6ZvX1UX1FR4EXEDUQh9aDUkOmlVRP"
 #SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
